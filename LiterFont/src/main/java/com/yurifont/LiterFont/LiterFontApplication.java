4a7f3fbf4ade18184eaa32b443fd6da239e8f9ef
YuriFont <yurifontenelesilva180303@gmail.com>
2024-06-11 20:50:01 -0300

    initial commit

---
+++ LiterFont/src/main/java/com/yurifont/LiterFont/LiterFontApplication.java
@@ -0,0 +1,13 @@
+package com.yurifont.LiterFont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterFontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterFontApplication.class, args);
+	}
+
+}
---
+++ LiterFont/src/test/java/com/yurifont/LiterFont/LiterFontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurifont.LiterFont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterFontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
